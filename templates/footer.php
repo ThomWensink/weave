01b8d23149dd62fc23d8caaa169e050ef2fd7c57
Thom Wensink <info@thomwensink.nl>
2019-11-21 14:43:19 -0500

    start update

---
+++ templates/footer.php
@@ -0,0 +1,27 @@
+<footer class="skewed">
+  	<div class="container">
+  		<div class="row">
+    		<div class="col-xs-12 col-sm-6">
+    			<?php dynamic_sidebar('footer-1'); ?> 
+    		</div>
+    		<div class="col-xs-12 col-sm-2">
+    			<?php dynamic_sidebar('footer-2'); ?>
+    		</div>
+    		<div class="col-xs-12 col-sm-4">
+    			<?php dynamic_sidebar('footer-3'); ?>
+    		</div>
+    	</div>
+  	</div>
+</footer>
+<section id="copyright">
+	<div class="container">
+  		<div class="row">
+  			<div class="col-xs-12 col-sm-6 text-left">
+  				Wmo Werkplaats Den Haag - Leiden © <?php echo date("Y"); ?>
+  			</div> 
+  			<div class="col-xs-12 col-sm-6 text-right"> 
+				Website by <a href="https://neurofied.com/">Neurofied</a></span>
+			</div>
+		</div>
+	</div>
+</section>
